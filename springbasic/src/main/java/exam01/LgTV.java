e77348235b0169421460aa3fae7edfa2bd8a8c4f
sgustjd2 <shin1133@naver.com>
2023-04-28 10:02:11 +0900

    no message

---
+++ springbasic/src/main/java/exam01/LgTV.java
@@ -0,0 +1,19 @@
+package exam01;
+
+public class LgTV {
+	public void turnOn() {
+		System.out.println("LgTV---? „?› ì¼ ë‹¤.");
+	}
+
+	public void turnOff() {
+		System.out.println("LgTV---? „?› ?ˆ?‹¤.");
+	}
+
+	public void soundUp() {
+		System.out.println("LgTV---?†Œë¦? ?˜¬ë¦°ë‹¤.");
+	}
+
+	public void soundDown() {
+		System.out.println("LgTV---?†Œë¦? ?‚´ë¦°ë‹¤.");
+	}
+}
---
+++ springbasic/src/main/java/exam01/SamsungTV.java
@@ -0,0 +1,19 @@
+package exam01;
+
+public class SamsungTV {
+	public void powerOn() {
+		System.out.println("SamsungTV---? „?› ì¼ ë‹¤.");
+	}
+
+	public void powerOff() {
+		System.out.println("SamsungTV---? „?› ?ˆ?‹¤.");
+	}
+
+	public void volumeUp() {
+		System.out.println("SamsungTV---?†Œë¦? ?˜¬ë¦°ë‹¤.");
+	}
+
+	public void volumeDown() {
+		System.out.println("SamsungTV---?†Œë¦? ?‚´ë¦°ë‹¤.");
+	}
+}
