ef78661ba9635fd049e4e32a8ba42656cc35ee47
sgustjd2 <shin1133@naver.com>
2023-05-10 17:54:30 +0900

    junit

---
+++ erp/src/test/java/com/multi/erp/JUnitTest01.java
@@ -0,0 +1,13 @@
+package com.multi.erp;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitTest01 {
+
+	@Test
+	public void test() {
+		assertEquals("1", "1");
+	}
+
+}
